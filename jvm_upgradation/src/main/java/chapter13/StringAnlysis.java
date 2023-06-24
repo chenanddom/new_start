a439b216923b82c8c1128103ca43aa35f042af54
chenanddom <772571631@qq.com>
2023-06-24 18:45:13 +0800

    提交字符串的相关代码和笔记

--- jvm_upgradation/src/main/java/chapter13/StringAnlysis.java
+++ jvm_upgradation/src/main/java/chapter13/StringAnlysis.java
@@ -5,7 +5,13 @@ package chapter13;
  */
 public class StringAnlysis {
     public static void main(String[] args) {
-
+        Object object = new Object();
+        StringAnlysis stringAnlysis = new StringAnlysis();
+        stringAnlysis.foo(object);
     }
 
+    public void foo(Object param) {
+        String str = param.toString();//会在内存中生成一个字符串存储在堆中
+        System.out.println(str);
+    }
 }
